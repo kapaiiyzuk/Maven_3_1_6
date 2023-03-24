ef4e248c0228e5d50f66e9fc796a83a59d8dc6d3
kapaiiyzuk <118076489+kapaiiyzuk@users.noreply.github.com>
2023-03-24 21:08:35 +0300

    Add files via upload

---
+++ Maven_3_1_6/src/main/java/kapaiiyzuk/rest_template/Config/RtConfig.java
@@ -0,0 +1,11 @@
+package kapaiiyzuk.rest_template.Config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+@ComponentScan("kapaiiyzuk.rest_template")
+public class RtConfig {
+}
